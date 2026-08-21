eaaf8a43229ae5c98aa1e4388a252802ab564a68
Anushka Saini <anushka0706b@gmail.com>
2026-08-21 09:22:47 +0530

    Added C++ programs

--- EXP1.cpp
+++ EXP1.cpp
@@ -1,59 +1,37 @@
-include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-void input(string &name, string &branch, string &section, int &roll, int marks[])
-{
-    cout << "Enter Name: ";
-    cin >> name;
 
-    cout << "Enter Branch: ";
-    cin >> branch;
+class Student {
+private:
+    int rollNo;
+    string name;
+    float marks;
 
-    cout << "Enter Section: ";
-    cin >> section;
+public:
+    void input() {
+        cout << "Enter Roll Number: ";
+        cin >> rollNo;
 
-    cout << "Enter Roll Number: ";
-    cin >> roll;
+        cout << "Enter Name: ";
+        cin >> name;
 
-    cout << "Enter Marks of 5 Subjects:\n";
-    for(int i = 0; i < 5; i++)
-    {
-        cout << "Subject " << i + 1 << ": ";
-        cin >> marks[i];
-    }
-}
-void findSum(int marks[], int &sum, float &percent)
-{
-    sum = 0;
-    for(int i=0; i < 5; i++)
-    {
-        sum +=marks[i];
-    }
-    percent = (sum /500.0) *100;
-}
-void show(string name, string branch, string section, int roll, int marks[], int sum, float percent)
-{
-    cout << "\n----- Student Details -----\n";
-    cout << "Name: " << name << endl;
-    cout << "Branch: " << branch << endl;
-    cout << "Section: " << section << endl;
-    cout << "Roll No.: " << roll << endl;
-
-    cout << "Marks: ";
-    for(int i = 0; i < 5; i++)
-    {
-        cout << marks[i] << " ";
+        cout << "Enter Marks: ";
+        cin >> marks;
     }
 
-    cout << "\nTotal Marks: " << sum << endl;
-    cout << "Percentage: " << percent << "%" << endl;
-    
-}
+    void display() {
+        cout << "\nStudent Record\n";
+        cout << "Roll Number: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
 
 int main() {
-    string,name,branch,section;
-    int roll, marks[5];
-    input(name,branch,section,roll,marks);
-    findSum(marks, sum, percent);
-    show(name,branch,section,roll,marks,sum,percent);
+    Student s;
+
+    s.input();
+    s.display();
+
     return 0;
 }
