12850cb0ddf7756b2d6886d5247d304e56b34e58
Anushka Saini <anushka0706b@gmail.com>
2026-09-25 09:53:54 +0530

    commit

---
+++ lab3/11.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+inline int add(int a, int b = 10)
+{
+    return a + b;
+}
+
+int subtract(int a, int b = 5)
+{
+    return a - b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+double multiply(double a, double b)
+{
+    return a * b;
+}
+
+int main()
+{
+    cout << "Addition: " << add(20) << endl;
+    cout << "Subtraction: " << subtract(20) << endl;
+    cout << "Multiplication (int): " << multiply(5, 4) << endl;
+    cout << "Multiplication (double): " << multiply(2.5, 4.0) << endl;
+
+    return 0;
+}
